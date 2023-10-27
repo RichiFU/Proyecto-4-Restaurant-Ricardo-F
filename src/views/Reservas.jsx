7802ce32595524e4b64e459c8089eb05b76248c5
RichiFU <andresurizarfuentes@gmail.com>
2023-10-27 03:48:36 -0300

    correcciones

--- src/views/Reservas.jsx
+++ src/views/Reservas.jsx
@@ -105,7 +105,7 @@ const Reservas = () => {
                     <div className="d-flex justify-content-center custom-reserva-image">
                         <div>
                             <Image
-                                src="../../public/mario-jump.png"
+                                src="../images/mario-jump.png"
                                 alt="Mario Jump"
                                 fluid
                                 className="mario-image-reserva"
